fcc73be611870ccf82175c62f0061b3b0c31335c
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-14 22:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const btn =document.getElementById("btn");
+let data,delay;
+function printText(data,delay){
+  let disp=document.getElementById("output");
+ return setTimeout(function() {
+    disp.textContent=data;
+  }, delay);
+}
+btn.addEventListener("click",(e)=>{
+  e.preventDefault();
+   data=document.getElementById("text").value;
+   delay=document.getElementById("delay").value;
+  answer();
+});
+async function answer(){
+	await printText(data,delay);
+}
